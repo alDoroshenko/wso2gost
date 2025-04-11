38b880a79fcd625775983e08c8a8adf79f6babcc
aldoroshenko <aldoroshenko@neoflex.ru>
2025-04-11 11:07:43 +0400

    test

--- src/main/java/org/example/issuer/CustomTokenIssuer.java
+++ src/main/java/org/example/issuer/CustomTokenIssuer.java
@@ -1,18 +1,12 @@
 package org.example.issuer;
 
 import org.apache.oltu.oauth2.common.exception.OAuthSystemException;
-import org.osgi.service.component.annotations.Component;
 import org.wso2.carbon.identity.oauth2.authz.OAuthAuthzReqMessageContext;
 import org.wso2.carbon.identity.oauth2.token.OAuthTokenReqMessageContext;
-import org.wso2.carbon.identity.oauth2.token.OauthTokenIssuer;
 import org.wso2.carbon.identity.oauth2.token.OauthTokenIssuerImpl;
 
-@Component(name = "custom.token.issuer", service = OauthTokenIssuer.class)
 public class CustomTokenIssuer extends OauthTokenIssuerImpl {
 
-    public CustomTokenIssuer() {
-
-    }
 
     @Override
     public String accessToken(OAuthAuthzReqMessageContext oauthAuthzMsgCtx) throws OAuthSystemException {
